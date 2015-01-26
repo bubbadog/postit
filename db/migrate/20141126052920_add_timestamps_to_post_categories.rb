a10b606213dfe76d8427c426f67304c9a36c8f7a
Bubbadog <justin.karbowski@gmail.com>
2015-01-26 10:40:05 -0800

    changed migration to :timestamp

--- db/migrate/20141126052920_add_timestamps_to_post_categories.rb
+++ db/migrate/20141126052920_add_timestamps_to_post_categories.rb
@@ -1,6 +1,6 @@
 class AddTimestampsToPostCategories < ActiveRecord::Migration
   def change
-    add_column :post_categories, :created_at, :timestamps
-    add_column :post_categories, :updated_at, :timestamps
+    add_column :post_categories, :created_at, :timestamp
+    add_column :post_categories, :updated_at, :timestamp
   end
 end
